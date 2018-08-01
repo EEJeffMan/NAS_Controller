1b28f74faa9fd46c3bebd2cd70a25d2c26e77f94
EEJeffMan <EE.JeffMan@gmail.com>
2018-07-31 22:08:31 -0500

    Reduced switching frequencies to 150kHz, and set to 33% duty cycle; created spice simulation for gate drive circuit.

--- Firmware/smps.h
+++ Firmware/smps.h
@@ -8,9 +8,9 @@
 #ifndef SMPS_H_
 #define SMPS_H_
 
-#define VAUX_PERIOD     240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
-#define PERIOD_5V2_A     240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
-#define PERIOD_5V2_B     240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
+#define VAUX_PERIOD     400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
+#define PERIOD_5V2_A     400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
+#define PERIOD_5V2_B     400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
 #define SEPIC_PERIOD     240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
 
 void init_smps();
