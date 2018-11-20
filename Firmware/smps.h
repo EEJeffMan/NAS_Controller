d3ba70a6955233da5e0eb3932f80a2b06d8ff63a
EEJeffMan <EE.JeffMan@gmail.com>
2018-11-19 20:14:50 -0600

    Added I2C code

--- Firmware/smps.h
+++ Firmware/smps.h
@@ -8,7 +8,7 @@
 #ifndef SMPS_H_
 #define SMPS_H_
 
-#define VAUX_PERIOD     400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
+#define VAUX_PERIOD     1000//400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
 #define PERIOD_5V2_A     400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
 #define PERIOD_5V2_B     400//240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
 #define SEPIC_PERIOD     240     // period = TBCLK period * VAUX_PERIOD - 240 = 250kHz
